2f30ef630918fcce393bdad0e9648f4a34883afe
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-07-30 23:47:24 +0900

    변수타입 복습중

---
+++ Dynamic/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name; //다이나믹으로 모든 변수형을 사용할수 있음
+  dynamic num; //이런식으로 다이나믹 명시로 선언할수 있음
+
+  name = 1;
+  name = false;
+  name = '데미안';
+  //다이나믹형이랑 모든 타입을 넣을수 있음
+
+  if (num is String) { // 또한 이런식으로 다이나믹을 특정 타입으로 유추해서 사용가능함
+    num. //String 타입에 맞게 자동완성 최적화됨
+  }
+  if(num is int){
+    num.
+  }
+}
---
+++ VarKeyword/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = '드미'; //var에는 초기값으로 아무거나 넣을수있음
+  //name = 1; //그러나 초기값 으로 넣은 변수형과 일치시켜서 사용해야함
+  //name = false; //초기값과 변수형이 다르면 오류뜸
+  name = '데미안';
+
+  String youName = 'you'; //명시적으로 타입을 지정할수도 있음
+  youName = '너';
+  print(name);
+  print(youName);
+}
