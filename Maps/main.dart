95a8be3c124b6331cfab9df6299d8652393dd42a
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-08-02 09:51:45 +0900

    add

--- Maps/main.dart
+++ Maps/main.dart
@@ -1,1 +1,7 @@
-void main() {}
+void main() {
+  var player = {'name': '제임스', 'level': 1, 'Ult': false};
+  Map<int, bool> skill = {1: true, 2: false, 3: true};
+  Map<List<int>, bool> tempList = {
+    [1, 2, 3, 4]: true,
+  };
+}
